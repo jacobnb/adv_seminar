aaf056a1af5dec7ddd59e8b5125261f3cf20e35e
Jacob <jacob.biederman@mymail.champlain.edu>
2018-08-30 20:42:36 -0400

    set up

---
+++ CubeStomp/Assets/Scripts/player_move.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_move : MonoBehaviour {
+    public float moveSpeed;
+    public float jumpHeight;
+    private float moveDirection;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        getInput();
+	}
+
+    void getInput()
+    {
+        moveDirection = Input.GetAxis("Horizontal");
+    }
+
+    void movePlayer()
+    {
+
+    }
+}
