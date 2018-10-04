f4bc15f02119e293ab7e9647eb71d2e1edd47768
Jacob <jacob.biederman@mymail.champlain.edu>
2018-10-04 11:07:31 -0400

    Set up TouchController / object architecture

---
+++ CubeStomp/Assets/Scripts/touch_controller_script.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Touchable;
+public class touch_controller_script : MonoBehaviour {
+    [SerializeField]
+    touch_object[] touchableObjects;
+    float[] object_radii; 
+    //not sure if it makes sense to cache the radii like this or not.
+
+
+	void Start () {
+        cacheRadii();
+    }
+	
+    
+	void Update () {
+	    //Iterate through touches	
+	}
+
+    void cacheRadii()
+    {
+        int index = 0;
+        foreach (touch_object touchObject in touchableObjects)
+        {
+            object_radii[index] = touchObject.getRadius();
+            index++;
+        }
+    }
+}
---
+++ CubeStomp/Assets/Scripts/touch_object.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Touchable
+{
+    public class touch_object : MonoBehaviour
+    {
+        [SerializeField]
+        float radius;
+        Touch myTouch;
+        
+        public float getRadius()
+        {
+            return radius;
+        }
+        public void setTouch(Touch newTouch)
+        {
+            myTouch = newTouch;
+        }
+    }
+}
+
