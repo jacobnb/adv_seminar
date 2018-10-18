c47681a3dcea6907bd46493685ff22a59213fba8
Jacob <jacob.biederman@mymail.champlain.edu>
2018-10-18 16:43:24 -0400

    Added basic dust anim

---
+++ CubeStomp/Assets/Scripts/destroy_after_load.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroy_after_load : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, 1f);
+	}
+
+}
---
+++ CubeStomp/Assets/Scripts/spawn_dust_cloud.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn_dust_cloud : MonoBehaviour {
+
+    [SerializeField]
+    GameObject dustCloud;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Instantiate(dustCloud, transform.position, dustCloud.transform.rotation);
+    }
+}
