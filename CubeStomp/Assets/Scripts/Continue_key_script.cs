34b6ef7559fe03d3e81c8da8570a511b456f3c5f
Jacob <jacob.biederman@mymail.champlain.edu>
2018-11-27 18:00:52 -0500

    Added keypress to continue script

---
+++ CubeStomp/Assets/Scripts/Continue_key_script.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+//This checks for a series of key presses and calls the given function if one is pressed.
+public class Continue_key_script : MonoBehaviour {
+    [SerializeField]
+    private UnityEvent funcToCall;
+    [SerializeField]
+    [Tooltip("When one of these is pressed it will trigger the function")]
+    private string[] keysToCheck;
+
+	// Update is called once per frame
+	void Update () {
+        getInput();
+	}
+
+    private void getInput()
+    {
+        foreach(string key in keysToCheck)
+        {
+            if (Input.GetKeyDown(key))
+            {
+                funcToCall.Invoke();
+                return;
+            }
+        }
+    }
+}
